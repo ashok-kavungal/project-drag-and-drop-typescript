b0306d661212d03bcd42709a02d618f637c16d8d
ashok <ashok.kavungal@tesa.com>
2020-04-24 16:27:13 +0200

    render html form to div

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('hello ashok')
+class InputData{
+    templeteEl : HTMLTemplateElement;
+    renderEL : HTMLDivElement;
+    element:HTMLFormElement;
+
+    constructor(){
+        this.templeteEl = document.getElementById("project-input")! as HTMLTemplateElement;
+        this.renderEL = document.getElementById("app")! as HTMLDivElement;
+        
+        const importedTemplateNode = document.importNode(this.templeteEl.content,true);
+        this.element = importedTemplateNode.firstElementChild! as HTMLFormElement
+        this.attatch();
+    }
+
+    private attatch(){
+        this.renderEL.insertAdjacentElement('afterbegin',this.element)
+    }
+}
+
+let testproject = new InputData();
