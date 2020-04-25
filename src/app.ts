3a45a9d87cf7c021c5f26347305cfb3790ed13a6
ashok <ashok.kavungal@tesa.com>
2020-04-25 14:49:45 +0200

    validating form input

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,36 @@
+interface validateParams{
+    value: string | number;
+    mandatory?: boolean;
+    minlen?: number;
+    maxlen?: number;
+    min?: number;
+    max?:number;
+}
+
+function validate(toValidateData: validateParams){
+    let isvalid = true;
+    if(toValidateData.mandatory){
+        isvalid = isvalid && toValidateData.value.toString().trim().length != 0;
+    }
+
+    if(typeof toValidateData.value ==='string' && toValidateData.minlen != null){
+        isvalid = isvalid &&  toValidateData.value.trim().length >= toValidateData.minlen
+    }
+
+    if(typeof toValidateData.value ==='string' && toValidateData.maxlen != null){
+        isvalid = isvalid &&  toValidateData.value.trim().length <= toValidateData.maxlen
+    }
+    
+    if(typeof toValidateData.value ==='number' && toValidateData.min != null){
+        isvalid = isvalid &&  toValidateData.value >= toValidateData.min
+    }
+
+    if(typeof toValidateData.value ==='number' && toValidateData.max!= null){
+        isvalid = isvalid &&  toValidateData.value <= toValidateData.max
+    }
+    return isvalid;
+}
+
 function autobind(
     _: any,
     _2 : string,
@@ -45,12 +78,56 @@ class InputData{
     @autobind
     private submitHandler(event:Event){
         event.preventDefault();
-        let temp = this.projectTitleEl.value;
-        console.log(temp);
+        const InputData = this.getValidatedData();
+        if(Array.isArray(InputData)){
+            const [title,desc,membors] = InputData;
+            console.log(title,desc,membors);
+            this.clearInputs();
+        }
+
+    }
+    private getValidatedData():[string,string,number]|void{
+        const title = this.projectTitleEl.value;
+        const desc = this.descriptionEl.value;
+        const memborNum = this.membersEl.value;
+        
+        const titleValidateParams : validateParams = {
+            value: title,
+            mandatory: true
+        }
+        const descValidateParams : validateParams = {
+            value: desc,
+            mandatory: true,
+            minlen : 5,
+            maxlen : 20
+        }
+        const memborValidateParams : validateParams = {
+            value: +memborNum,
+            mandatory: true,
+            min:1,
+            max:5
+        }
+
+        if(
+            !validate(titleValidateParams) ||
+            !validate(descValidateParams) ||
+            !validate(memborValidateParams)
+        ){
+            alert('invalid form inputs');
+            return;
+        }else{
+            return [ title,desc,+memborNum];
+        }
     }
 
+    private clearInputs() {
+        this.projectTitleEl.value = '';
+        this.descriptionEl.value = '';
+        this.membersEl.value = '';
+      }
+
     private configSubmitEvent(){
-        this.element.addEventListener('submit',this.submitHandler;
+        this.element.addEventListener('submit',this.submitHandler);
     }
 
     private attatch(){
